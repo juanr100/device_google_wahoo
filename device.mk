ed1e6ab3299707f54620a58ea689a02ea74490f7
Juan <jramos69@gmail.com>
2020-04-22 17:11:14 -0400

    Enable EUICC Sim
    Change-Id: I57c20284fd2a00b5155706838ad46bbb8355adcf

--- device.mk
+++ device.mk
@@ -731,3 +731,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/fstab.postinstall:$(TARGET_COPY_OUT_PRODUCT)/etc/fstab.postinstall
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
