98b8ac6c45f54fe5024324a6eac3905ecc8a33b4
Juan <jramos69@gmail.com>
2020-04-22 17:12:10 -0400

    IMS Support
    Build what we can
    
    Change-Id: I3baf138af6d5750fe55b06cb379fc98cc7eee365

--- device.mk
+++ device.mk
@@ -734,3 +734,9 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
+
+# IMS
+PRODUCT_PACKAGES += \
+    com.android.ims.rcsmanager \
+    RcsService \
+    PresencePolling
